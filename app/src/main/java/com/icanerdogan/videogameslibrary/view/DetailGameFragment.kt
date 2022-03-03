eb14d149da5f062e5002a4985c1ab58f3fc0a257
icanerdogan <ibrahim4545erdogan@gmail.com>
2022-03-03 18:57:32 +0300

    detail fragment redesign

--- app/src/main/java/com/icanerdogan/videogameslibrary/view/DetailGameFragment.kt
+++ app/src/main/java/com/icanerdogan/videogameslibrary/view/DetailGameFragment.kt
@@ -1,6 +1,7 @@
 package com.icanerdogan.videogameslibrary.view
 
 import android.os.Bundle
+import android.text.method.ScrollingMovementMethod
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -18,16 +19,20 @@ class DetailGameFragment : Fragment() {
     private lateinit var detailGameViewModel: DetailGameViewModel
     private var getClickedBasicGameID : Int = 0
 
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+
         binding = FragmentDetailGameBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.detailGameTextViewDescription.movementMethod = ScrollingMovementMethod()
 
         arguments?.let {
             getClickedBasicGameID = DetailGameFragmentArgs.fromBundle(it).detailGameID
